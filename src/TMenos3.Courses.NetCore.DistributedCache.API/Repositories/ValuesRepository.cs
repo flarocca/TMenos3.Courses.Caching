5c03f7f65d2d0d229eca7926cf2d9ca1fd034c43
facu-larocca-zenfolio <flarocca@zenfolio.com>
2020-01-09 23:43:19 -0300

    API implemented

---
+++ src/TMenos3.Courses.NetCore.DistributedCache.API/Repositories/ValuesRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TMenos3.Courses.NetCore.DistributedCache.API.Repositories
+{
+    public class ValuesRepository : IValuesRepository
+    {
+        public async Task<IEnumerable<string>> GetAllAsync()
+        {
+            await Task.Delay(1000);
+
+            return new List<string>
+            {
+                "Value 1",
+                "Value 2"
+            };
+        }
+    }
+}
